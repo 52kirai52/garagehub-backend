15ad9af02a513ecebaa7dda74914cb39f7c03035
김상윤 <52kirai52@gmail.com>
2026-06-22 22:28:25 +0900

    init: project setup

---
+++ src/main/java/com/garagehub/garagehub/GaragehubApplication.java
@@ -0,0 +1,13 @@
+package com.garagehub.garagehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garagehub/garagehub/GaragehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garagehub.garagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
