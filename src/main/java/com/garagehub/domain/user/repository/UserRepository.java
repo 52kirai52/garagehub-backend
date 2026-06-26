14ea389b7537c097b1ea74464368eac2ac234497
52kirai52 <52kirai52@gmail.com>
2026-06-26 20:49:50 +0900

    feat: UserRepository 생성

---
+++ src/main/java/com/garagehub/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.garagehub.domain.user.repository;
+
+import com.garagehub.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByPhone(String phone);
+    boolean existsByUsername(String username);
+}
