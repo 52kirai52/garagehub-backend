5ac15d8914a41b665c73350044d455b8767fc859
52kirai52 <52kirai52@gmail.com>
2026-07-07 15:26:36 +0900

    feat: 인증번호 불일치 에러코드(INVALID_CODE) 추가

--- src/main/java/com/garagehub/global/exception/ErrorCode.java
+++ src/main/java/com/garagehub/global/exception/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     SMS_LOCK(HttpStatus.BAD_REQUEST, "AUTH_002", "요청을 처리 중입니다. 잠시 후 다시 시도해주세요."),
     SMS_COOLDOWN(HttpStatus.BAD_REQUEST, "AUTH_003", "잠시 후 다시 시도해주세요."),
     SMS_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_004", "인증번호 발송에 실패했습니다."),
+    SMS_INVALID_CODE(HttpStatus.BAD_REQUEST, "AUTH_005", "인증번호가 올바르지 않습니다."),
 
     // 공통 (COMMON)
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON_001", "올바른 입력값이 아닙니다."),
