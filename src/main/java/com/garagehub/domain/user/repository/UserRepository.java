17246dec152fa1ea39a440e4523ace0a449b85b9
52kirai52 <52kirai52@gmail.com>
2026-07-09 12:26:43 +0900

    feat: 이메일 중복 확인 메서드 추가

--- src/main/java/com/garagehub/domain/user/repository/UserRepository.java
+++ src/main/java/com/garagehub/domain/user/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByPhone(String phone);
     boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
