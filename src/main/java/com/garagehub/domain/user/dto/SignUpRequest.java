a8cde79f739f0b38189aca04064eb27c51654ca6
52kirai52 <52kirai52@gmail.com>
2026-07-09 12:53:43 +0900

    refactor: 회원가입 유효성 검증 메시지 간소화

--- src/main/java/com/garagehub/domain/user/dto/SignUpRequest.java
+++ src/main/java/com/garagehub/domain/user/dto/SignUpRequest.java
@@ -15,22 +15,22 @@ public class SignUpRequest {
 
     @NotBlank
     @Pattern(regexp = "^[a-z][a-z0-9]{3,19}$", 
-             message = "아이디는 영문 소문자로 시작하고 영문 소문자, 숫자만 사용 가능합니다. (4~20자)")
+             message = "아이디 형식이 올바르지 않습니다.")
     private String username;
 
     @NotBlank
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-             message = "비밀번호는 대소문자, 숫자, 특수문자를 포함한 8자 이상이어야 합니다.")
+             message = "비밀번호 형식이 올바르지 않습니다.")
     private String password;
 
     @NotBlank
     @Pattern(regexp = "^[가-힣a-zA-Z]{2,100}$",
-             message = "이름은 한글 또는 영문만 사용 가능합니다. (2~100자)")
+             message = "이름 형식이 올바르지 않습니다.")
     private String name;
 
     @NotBlank
     @Pattern(regexp = "^01[0-9]{8,9}$", 
-             message = "올바른 전화번호 형식이 아닙니다.")
+             message = "전화번호 형식이 올바르지 않습니다..")
     private String phone;
 
     @NotNull(message = "생년월일을 입력해주세요.")
